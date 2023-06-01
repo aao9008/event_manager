4ffc0933154c0b387b093fd0873ece94d0a79c7e
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-06-01 18:38:03 -0400

    Add Iteration 0: Load a CSV file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,11 +5,12 @@ puts(File.exist? 'event_attendees.csv')
 
 # read csv file contents
 contents = File.read('event_attendees.csv')
-puts contents
+# puts contents
 
 # save lines into array then output contents of each line
 lines = File.readlines('event_attendees.csv')
 
+=begin
 lines.each do |line|
   puts line
 end 
@@ -21,4 +22,31 @@ lines.each do |line|
   name = columns[2]
   puts name
 end 
+=end
+
+# Skip header row 
+
+=begin
+row_index = 0
+
+ lines.each do |line|
+  row_index += 1
+
+  next if row_index == 1
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end 
+=end 
+
+# Second option
+
+lines.each_with_index do |line,index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
+
+# Iteration 1: Parsing with CSV
 
