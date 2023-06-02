83053b46ba6fcb90ce2a808129e1e27fc6a425a3
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-06-01 21:58:08 -0400

    Add Iteration 1: Parsing with CSV

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,11 @@
+require 'csv'
+
 puts 'Event Manager Initialized!'
 
 # check if file exists
 puts(File.exist? 'event_attendees.csv')
 
+=begin
 # read csv file contents
 contents = File.read('event_attendees.csv')
 # puts contents
@@ -10,7 +13,7 @@ contents = File.read('event_attendees.csv')
 # save lines into array then output contents of each line
 lines = File.readlines('event_attendees.csv')
 
-=begin
+
 lines.each do |line|
   puts line
 end 
@@ -22,11 +25,11 @@ lines.each do |line|
   name = columns[2]
   puts name
 end 
-=end
+
 
 # Skip header row 
 
-=begin
+
 row_index = 0
 
  lines.each do |line|
@@ -37,7 +40,7 @@ row_index = 0
   name = columns[2]
   puts name
 end 
-=end 
+
 
 # Second option
 
@@ -47,6 +50,39 @@ lines.each_with_index do |line,index|
   name = columns[2]
   puts name
 end
+=end
 
 # Iteration 1: Parsing with CSV
 
+# Use Ruby CSV parser to access attendees' zip code.
+
+contents = CSV.open('event_attendees.csv', headers: true)
+
+=begin
+contents.each do |row|
+  name = row[2]
+  puts name
+end 
+=end
+
+# Accessing Columns by their Names
+
+contents = CSV.open('event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+=begin
+contents.each do |row|
+  name = row[:first_name]
+  puts name
+end
+=end
+
+# Displaying the Zip Codes of All Attendees
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
+end
