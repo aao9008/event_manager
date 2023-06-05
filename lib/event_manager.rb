69ed2c36bdc0094119bb2af6a8e077c574549894
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-06-05 01:09:41 -0400

    Add completed Iteration 2:Cleaning up our Zip Codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,12 @@ require 'csv'
 
 puts 'Event Manager Initialized!'
 
+puts Dir.pwd
+
+Dir.chdir '/Users/alfredo.ormeno/repos/event_manager'
+
+puts Dir.pwd
+
 # check if file exists
 puts(File.exist? 'event_attendees.csv')
 
@@ -55,10 +61,10 @@ end
 # Iteration 1: Parsing with CSV
 
 # Use Ruby CSV parser to access attendees' zip code.
-
+=begin
 contents = CSV.open('event_attendees.csv', headers: true)
 
-=begin
+
 contents.each do |row|
   name = row[2]
   puts name
@@ -80,9 +86,76 @@ end
 =end
 
 # Displaying the Zip Codes of All Attendees
-
+=begin
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  # Iteration 2: Cleaning up our Zip Codes
+
+  # if the zip code is exactly five digits, assume that it is okay, so do nothing
+
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif  zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+
+  # if the zip code is more than five digits, truncate it to the first five digits.
+  elsif zipcode.length > 5
+    zipcode = zipcode[0,5]
+  end
+
+  puts "#{name} #{zipcode}"
+end
+=end
+
+=begin Move clean zip code to its own method 
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif  zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+
+  # if the zip code is more than five digits, truncate it to the first five digits.
+  elsif zipcode.length > 5
+    zipcode = zipcode[0, 5]
+  else
+    zipcode
+  end
+end
+
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+end
+=end
+
+# Refactoring clean_zipcodes
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
