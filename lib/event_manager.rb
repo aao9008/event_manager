2d88f9c50e3b68ab5ace8acf3094fbf3da95107c
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-06-08 00:03:53 -0400

    Add Iteration 4: Form Letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 puts 'Event Manager Initialized!'
 
@@ -25,29 +26,42 @@ def legislators_by_zipcode(zipcode)
 
   # use exception class to rescue a bad zip code query request
   begin
-    legislators = civic_info.representative_info_by_address(address: zipcode, levels: 'country', roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-
-    legislators = legislators.officials
-
-    # Use map function to tranform data in legislator object to dispay array of legislator names
-    legislator_names = legislators.map(&:name)
-
-    # Create comma seperate list from array to cleanly display names
-    legislators_string = legislator_names.join(", ")
+    legislators = civic_info.representative_info_by_address(address: zipcode, levels: 'country', roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
   rescue
-    puts 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
+
+def save_thankyou_letter(id, form_letter)
+  # create folder for custom form letters if folder does not exist
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  # Create file for each attendee, ID value identifies attendee
+  filename = "output/thanks_#{id}.html"
+
+  # Create and open new file for attendee
+  File.open(filename, 'w') do |file|
+    # copt erb output to custom form letter file 
+    file.puts form_letter
+  end
+end
+
 
 # Open CSV file
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thankyou_letter(id, form_letter)
 end
